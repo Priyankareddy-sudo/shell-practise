e155512a93e05eadf098e1f3cf0280d22dde0489
Priyankareddy-sudo <reddypriyanka467@gmail.com>
2026-06-30 16:43:40 +0530

    shell scripting practise

--- special_variables.sh
+++ special_variables.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 echo "special variables passed through the script are: $@"
 echo "the number of arguments passed is: $#"
+echo "the name of the script is: $0"
+echo "the first argument is: $1"
+echo "the second argument is: $2"
+echo "the third argument is: $3"

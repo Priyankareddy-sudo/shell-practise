d3c97eac14b9b51158c8dbc64c7a8ef44938d075
Priyankareddy-sudo <reddypriyanka467@gmail.com>
2026-06-30 18:22:41 +0530

    shell scripting practise

--- condition.sh
+++ condition.sh
@@ -2,4 +2,8 @@
 number=$1
 if [ $number -gt 20 ]; then
     echo "$number is a greater number."
+    elif [ $number -eq 20 ]; then
+    echo "$number is equal to 20."
+    else
+    echo "$number is a smaller number."
 fi

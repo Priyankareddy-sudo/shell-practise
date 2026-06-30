b3bc9804eb1b0538a4cb6387c0a4917954c8ef4e
Priyankareddy-sudo <reddypriyanka467@gmail.com>
2026-06-30 14:26:40 +0530

    shell scripting practise

--- timestamp.sh
+++ timestamp.sh
@@ -2,7 +2,7 @@
 timestamp=$(date)
 echo "time is $timestamp"
 start_time=$(date +%s)
-sleep 10seconds
+sleep 10 seconds
 end_time=$(date +%s)
 execution_time=$((end_time - start_time))
 echo "Execution time: $execution_time seconds"

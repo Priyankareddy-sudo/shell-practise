2907b95b2467a6feded289967404ed8c4a1ffdff
Priyankareddy-sudo <reddypriyanka467@gmail.com>
2026-06-30 17:08:50 +0530

    shell scripting practise

--- special_variables.sh
+++ special_variables.sh
@@ -7,11 +7,20 @@ echo "the second argument is: $2"
 echo "the third argument is: $3"
 echo "who is running the script: $USER"
 echo "the process id of the script is: $$"
+sleep 5
 echo "the exit status of the last command is: $?"
+sleep 5
 echo "the last background process id is: $!"
 echo "the current shell is: $SHELL"
 echo "the current working directory is: $PWD"
 echo "the home directory of the user is: $HOME"
 echo "the current date and time is: $(date)"
+sleep 5
 echo "the current time in seconds since epoch is: $(date +%s)"
+sleep 5
 echo "the current time in seconds since epoch is: $(date +%s.%N)"
+echo "line number of the script is: $LINENO"
+echo "the last command executed is: $BASH_COMMAND"
+echo "script eeexecution started at: $SECONDS seconds since the script started"
+echo "random number generated is: $RANDOM"
+
